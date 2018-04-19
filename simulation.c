c1a94e00f3e01a34c9c6ec2b7f81825e0345bf2f
Andy <36960463+bonnetto@users.noreply.github.com>
2018-04-19 12:59:09 +0200

    Update simulation.c

--- simulation.c
+++ simulation.c
@@ -76,19 +76,19 @@ bool simulation_colision_robot_particule()
 	return 1;
 }
 
-int simulation_get_nb_robots (nb_robots)
+int simulation_get_nb_robots ()
 {
 	return nb_robots;
 }
-int simulation_get_nb_particules (nb_particules)
+int simulation_get_nb_particules ()
 {
 	return nb_particules;
 }
-double *simulation_get_robots (pointer_r)
+double *simulation_get_robots ()
 {
 	return pointer_r;
 }
-double *simulation_get_particules (pointer_p)
+double *simulation_get_particules ()
 {
 	return pointer_p;
 }
